f78f45514ea31fcf2d00b2281df186213a5f4232
Moritz Eysholdt <moritz@gitpod.io>
2024-12-12 14:26:20 +0000

    updates

--- generate.sh
+++ generate.sh
@@ -6,5 +6,5 @@ mkdir -p $D
 
 # Create 100 binary files
 for i in {1..100}; do
-    dd if=/dev/zero of=$D/file${i}.bin bs=1M count=10 >/dev/null 2>&1
+    dd if=/dev/zero of=$D/file${i}.bin bs=100k count=10 >/dev/null 2>&1
 done
